05e4c77d33b69668a2301a9e89fd4223faf9d06c
Your Name <you@example.com>
2026-02-20 10:14:16 +0530

    Added Use Case 1 - Welcome Message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+/**
+ * MAIN CLASS – UseCase1PalindromeApp
+ * Use Case 1: Application Entry & Welcome Message
+ * @author Developer
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Palindrome Checker ");
+        System.out.println(" Version 1.0 ");
+        System.out.println("=================================");
+
+    }
+}

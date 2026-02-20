525e7acaa2df7f62f03fd8f0933383d959347780
Your Name <you@example.com>
2026-02-20 10:31:15 +0530

    Added Use Case 2 - Hardcoded Palindrome Validation

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -9,10 +9,27 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println(" Welcome to Palindrome Checker ");
-        System.out.println(" Version 1.0 ");
-        System.out.println("=================================");
+        System.out.println("Palindrome Checker - Version 2.0");
+        System.out.println("----------------------------------");
 
+        // Hardcoded string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
     }
 }
